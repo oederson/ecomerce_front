4e3e8bb0733919968f66a4e7c8e45d880a86bb39
oederson <eder.hu@live.com.pt>
2023-06-23 14:38:30 -0300

    redux revisado

--- src/redux/store.js
+++ src/redux/store.js
@@ -31,6 +31,7 @@ export const store = configureStore ({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
+      devTools: false,
     }),
 });
 
