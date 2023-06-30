0cdffdc8f10a8e7564ac9d873dd7337fabe13ef0
oederson <eder.hu@live.com.pt>
2023-06-30 15:44:29 -0300

    nginx sem config s

--- src/services/metodoRequest.js
+++ src/services/metodoRequest.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-//const bkeURI =  "localhost:8080" //import.meta.env.VITE_API_URL;
+const bkeURI =  'https://backend-production-2fa9.up.railway.app/'//"localhost:8080" //import.meta.env.VITE_API_URL;
 
 const ChamadaApi = (token) => {
   
   const tipoResquisiçao = () =>{
     if(token){
         return axios.create({
-            baseURL: "/api",
+            baseURL: bkeURI,
             headers: {
               Authorization: token,
             },
           });
     }else{
         return axios.create({
-            baseURL: "/api",
+            baseURL: bkeURI,
         });
         }
   }
