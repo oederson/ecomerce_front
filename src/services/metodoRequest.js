db46c3a7e527b3d4b0f75505767b2e9a0d28bfcf
oederson <eder.hu@live.com.pt>
2023-06-30 15:33:01 -0300

    nginx teste proxy reverso

--- src/services/metodoRequest.js
+++ src/services/metodoRequest.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-const bkeURI =  "localhost:8080" //import.meta.env.VITE_API_URL;
+//const bkeURI =  "localhost:8080" //import.meta.env.VITE_API_URL;
 
 const ChamadaApi = (token) => {
   
   const tipoResquisiçao = () =>{
     if(token){
         return axios.create({
-            baseURL: bkeURI,
+            baseURL: "/api",
             headers: {
               Authorization: token,
             },
           });
     }else{
         return axios.create({
-            baseURL: bkeURI,
+            baseURL: "/api",
         });
         }
   }
