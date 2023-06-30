fd4a151687c359dcf7f637458f3f1d2a40b66c9a
oederson <eder.hu@live.com.pt>
2023-06-30 15:36:15 -0300

    nginx teste proxy reverso

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
+  server : {proxy: {
+    '/api': {
+         target: 'https://backend-production-2fa9.up.railway.app/',
+         changeOrigin: true,
+         secure: false,      
+         ws: false,
+         rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+  }}
 });
