c7f34732e18c665ce06ae62e5a2fd0a6cec235c2
oederson <eder.hu@live.com.pt>
2023-06-30 15:47:12 -0300

    nginx sem config s

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server : {proxy: {
-    '/api': {
-         target: 'https://backend-production-2fa9.up.railway.app/',
-         changeOrigin: true,
-         secure: false,      
-         ws: false,
-         rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-  }}
 });
